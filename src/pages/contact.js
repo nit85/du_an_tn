3ce493b4f34c8526fdb2907d6ea475d32cc97b57
tai <thironman85@gmail.com>
2021-11-30 00:56:06 +0700

    first commit

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,7 +7,9 @@ import TextField from '@mui/material/TextField';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
-
+import image1 from '../images/slide1.jpg'
+import image from '../images/slide2.jpg'
+import image3 from '../images/slide3.jpg'
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
@@ -93,101 +95,16 @@ const Contact = () => {
                     autoComplete="mess"
                   />
                   <iframe title="reCAPTCHA" src="https://www.google.com/recaptcha/api2/anchor?ar=1&amp;k=6LcQLnYUAAAAAMhMj1PkbR2aSkQlG2CJzhdBVSlr&amp;co=aHR0cHM6Ly93d3cub3JnYW5pY3RhdHR2YS5jb206NDQz&amp;hl=vi&amp;v=qljbK_DTcvY1PzbR7IG69z1r&amp;size=normal&amp;cb=pbcqbdbtbxf6" width="304" height="78" role="presentation" name="a-lr6zcsw1rrhd" frameborder="0" scrolling="no" sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-top-navigation allow-modals allow-popups-to-escape-sandbox"></iframe>
-                  
-                  <button type="submit" class="btn btn-dark">Gửi</button>    
+
+                  <button type="submit" class="btn btn-dark">Gửi</button>
                 </Box>
               </Box>
             </Container>
           </form>
         </div>
       </section>
-<div className="footcontact">
 
-      <div class="p-3 mb-2 bg-dark text-white">
-        <div class="col">
-          <p>
-            <b>Tất cả sản phẩm</b>
-          </p>
-          <p>
-            <a href="#" > Organic Rice </a>
-            |
-            <a href="#" > Organic Cereals & Grains </a>
-            |
-            <a href="#" > Organic Flours </a>
-            |
-            <a href="#" > Organic Pulses </a>
-            |
-            <a href="#" > Organic Sugar & Jaggery </a>
-            |
-            <a href="#" > Organic Whole Spices </a>
-            |
-            <a href="#" > Organic Masala Blends </a>
-            |
-            <a href="#" > Organic Super Foods & Health Foods </a>
-            |
-            <a href="#" > Organic Tea & Coffee </a>
-            |
-            <a href="#" > Organic Oils, Honey & Pastes </a>
-            |
-            <a href="#" > Natural Salts </a>
-            |
-            <a href="#" > Organic Ground Spices </a>
-            |
-            <a href="#" > Organic Dry Fruits </a>
-            |
-          </p>
-        </div>
-
-      <div >
-          <hr />
-          <div class="footer-top1 col-lg-3 col-md-3 col-sm-3 col-xs-12">
-            <aside id="custom_html-5" class="widget_text widget widget_custom_html"><h5 class="wg-title">About Us</h5><div class="textwidget custom-html-widget">Mehrotra Consumer Products Pvt. Ltd. is a young and dynamic company aspiring to be a leader in the Organic food industry.
-
-              <h5 class="wg-title two">Like &amp; Share</h5>
-
-              <a class="navbar-brand" href="https://www.facebook.com/">
-                <FaFacebookF />
-              </a>
-              <a class="navbar-brand" href="https://www.instagram.com/" >
-                <FiInstagram />
-              </a>
-              <a class="navbar-brand" href="https://twitter.com/i/flow/login?input_flow_data=%7B%22requested_variant%22%3A%22eyJyZWRpcmVjdF9hZnRlcl9sb2dpbiI6Imh0dHBzOi8vZGV2ZWxvcGVyLnR3aXR0ZXIuY29tL2VuL2FwcHMifQ%3D%3D%22%7D" >
-                <FaTwitter />
-              </a>
-
-            </div>
-            </aside>
-          </div>
-          <div class="footer-top2 col-lg-3 col-md-3 col-sm-3 col-xs-12">
-            <aside id="custom_html-4" class="widget_text widget widget_custom_html"><h5 class="wg-title">Get in Touch</h5><div class="textwidget custom-html-widget"><p>Mehrotra Consumer Products Pvt. Ltd.
-              26G, Sector 31, Ecotech 1, <br />
-              Greater Noida-201308, India</p>
-              <br />
-              <p><a href="mailto:customercare@organictattva.com" class="link-ho">customercare@organictattva.com</a></p>
-              <p><a href="tel:+91 120 4260545" class="link-ho">+91 120 4260545</a></p></div></aside>
-          </div>
-
-          <div class="footer-top3 col-lg-3 col-md-3 col-sm-3 col-xs-12">
-            <aside id="custom_html-3" class="widget_text widget widget_custom_html"><h5 class="wg-title">Quick Links</h5><div class="textwidget custom-html-widget"><p><a href="https://www.organictattva.com/">Trang chủ</a></p>
-             
-              <p><a href="/product">Sản Phẩm</a></p>
-              <p><a href="/contact">Liên Hệ</a></p>
-              <p><a href="/new">Tin Tức</a></p>
-
-            </div>
-            </aside>
-          </div>
-
-          <div class="footer-top4 col-lg-3 col-md-3 col-sm-3 col-xs-12">
-            <aside id="custom_html-2" class="widget_text widget widget_custom_html"><h5 class="wg-title">Useful Links</h5><div class="textwidget custom-html-widget"><p><a href="https://www.organictattva.com/terms-conditions/">Terms &amp; Conditions</a></p>
-              <p><a href="https://www.organictattva.com/privacy-policy/">Privacy Policy</a></p>
-              <p><a href="https://www.organictattva.com/sitemap">Sitemap</a></p>
-              <p><a href="https://www.organictattva.com/faqs/">FAQ's</a></p>
-              <p><a href="https://www.organictattva.com/buy-now/">Buy Now</a></p></div></aside>
-          </div>
-</div>
-        </div>
-</div>
+     
     </div>
   );
 };
--- src/pages/home.js
+++ src/pages/home.js
@@ -5,11 +5,12 @@ import image3 from '../images/slide3.jpg'
 import { carousel } from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../page_css/home.css';
-import { FaFacebookF, FaTwitter } from 'react-icons/fa';
-import { FiInstagram } from "react-icons/fi";
+import { AiFillMail, AiFillPhone,AiFillInstagram  } from 'react-icons/ai';
+import { FiTwitter } from 'react-icons/fi';
+
 const Home = () => {
   return (
-    <div>
+    <div class="home">
       <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-indicators">
           <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
@@ -119,129 +120,88 @@ const Home = () => {
       </div>
 
 
-      <div class="vc_empty_space" >
-        <span class="vc_empty_space_inner"></span>
-      </div>
-      <h1 class="vc_custom_heading vc_custom_1596083600508">
-        <span>Resipes</span></h1>
-      <div class="wpb_single_image wpb_content_element vc_align_center wpb_animate_when_almost_visible wpb_fadeInUp fadeInUp mobi-heading wpb_start_animation animated">
-        <figure class="wpb_wrapper vc_figure">
-          <div class="vc_single_image-wrapper   vc_box_border_grey"><img width="150" height="22" src="https://www.organictattva.com/wp-content/uploads/2018/07/line-150x22.png" class="vc_single_image-img attachment-thumbnail" alt="line" loading="lazy" title="line" /></div>
-        </figure>
-      </div>
-      <a href="https://www.organictattva.com/recipes/amaranth-cutlet/" target="_self" class="vc_single_image-wrapper vc_box_shadow_3d  vc_box_border_grey">
-        <img width="360" height="240" src="https://www.organictattva.com/wp-content/uploads/2019/08/Amaranth-cutlet.jpg" class="vc_single_image-img attachment-large" alt="Organic Tattva Amaranth Cutlet Recipe" loading="lazy" srcset="https://www.organictattva.com/wp-content/uploads/2019/08/Amaranth-cutlet.jpg 360w, https://www.organictattva.com/wp-content/uploads/2019/08/Amaranth-cutlet-300x200.jpg 300w" sizes="(max-width: 360px) 100vw, 360px" title="Organic Tattva Amaranth Cutlet Recipe" /></a>
-     
-      <div className="recipes"> 
-        <span><a href="http://www.organictattva.com/recipes/amaranth-cutlet/">Amaranth Cutlet</a></span>
-      </div>
 
-
-      <div class="wpb_single_image wpb_content_element vc_align_left">
-        <figure class="wpb_wrapper vc_figure">
-          <a href="https://www.organictattva.com/recipes/biryani/" target="_self" class="vc_single_image-wrapper vc_box_shadow_3d  vc_box_border_grey">
-            <img className="im1" width="360" height="240" src="https://www.organictattva.com/wp-content/uploads/2019/08/Biryani.jpg" class="vc_single_image-img attachment-large" alt="Biryani Recipe by Organic Tattva" loading="lazy" srcset="https://www.organictattva.com/wp-content/uploads/2019/08/Biryani.jpg 360w, https://www.organictattva.com/wp-content/uploads/2019/08/Biryani-300x200.jpg 300w" sizes="(max-width: 360px) 100vw, 360px" title="Biryani Recipe by Organic Tattva" /></a>
-        </figure>
-      </div>
-      <span className="recipes"><a href="http://www.organictattva.com/recipes/biryani/">Biryani</a></span>
-
-      <div class="wpb_single_image wpb_content_element vc_align_left">
-        <figure class="wpb_wrapper vc_figure">
-          <a href="https://www.organictattva.com/recipes/suji-barfi/" target="_self" class="vc_single_image-wrapper vc_box_shadow_3d  vc_box_border_grey">
-            <img id="h1" width="360" height="240" src="https://www.organictattva.com/wp-content/uploads/2019/08/Suji-Barfi.jpg" class="vc_single_image-img attachment-large" alt="Suji Barfi Recipe by Organic Tattva" loading="lazy" srcset="https://www.organictattva.com/wp-content/uploads/2019/08/Suji-Barfi.jpg 360w, https://www.organictattva.com/wp-content/uploads/2019/08/Suji-Barfi-300x200.jpg 300w" sizes="(max-width: 360px) 100vw, 360px" title="Suji Barfi Recipe by Organic Tattva" /></a>
-        </figure>
-       
-      </div>
       
+      
+      <footer class="footer-32892 pb-0">
+      <div class="site-section">
+        <div class="container">
 
+          
+          <div class="row">
 
+            <div class="col-md pr-md-5 mb-4 mb-md-0">
+              <h3>About Us</h3>
+              <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam itaque unde facere repellendus, odio et iste voluptatum aspernatur ratione mollitia tempora eligendi maxime est, blanditiis accusamus. Incidunt, aut, quis!</p>
+              <ul class="list-unstyled quick-info mb-4">
+              <li> <a href="#"  class="d-flex align-items-center"> <span class="icon mr-3 icon-phone"></span> <AiFillPhone />  +1 291 3912 329</a>  </li>
+                <li>  <a href="https://mail.google.com/" class="d-flex align-items-center"> <span class="icon mr-3 icon-envelope"></span><AiFillMail /> info@gmail.com</a></li>
+              </ul>
 
-
-      <div class="p-3 mb-2 bg-dark text-white">
-        <div class="col">
-          <p>
-            <b>Tất cả sản phẩm</b>
-          </p>
-          <p>
-            <a href="#" > Organic Rice </a>
-            |
-            <a href="#" > Organic Cereals & Grains </a>
-            |
-            <a href="#" > Organic Flours </a>
-            |
-            <a href="#" > Organic Pulses </a>
-            |
-            <a href="#" > Organic Sugar & Jaggery </a>
-            |
-            <a href="#" > Organic Whole Spices </a>
-            |
-            <a href="#" > Organic Masala Blends </a>
-            |
-            <a href="#" > Organic Super Foods & Health Foods </a>
-            |
-            <a href="#" > Organic Tea & Coffee </a>
-            |
-            <a href="#" > Organic Oils, Honey & Pastes </a>
-            |
-            <a href="#" > Natural Salts </a>
-            |
-            <a href="#" > Organic Ground Spices </a>
-            |
-            <a href="#" > Organic Dry Fruits </a>
-            |
-          </p>
-        </div>
-
-        <div>
-          <hr />
-          <div class="footer-top1 col-lg-3 col-md-3 col-sm-3 col-xs-12">
-            <aside id="custom_html-5" class="widget_text widget widget_custom_html"><h5 class="wg-title">About Us</h5><div class="textwidget custom-html-widget">Mehrotra Consumer Products Pvt. Ltd. is a young and dynamic company aspiring to be a leader in the Organic food industry.
-
-              <h5 class="wg-title two">Like &amp; Share</h5>
-
-              <a class="navbar-brand" href="https://www.facebook.com/">
-                <FaFacebookF />
-              </a>
-              <a class="navbar-brand" href="https://www.instagram.com/" >
-                <FiInstagram />
-              </a>
-              <a class="navbar-brand" href="https://twitter.com/i/flow/login?input_flow_data=%7B%22requested_variant%22%3A%22eyJyZWRpcmVjdF9hZnRlcl9sb2dpbiI6Imh0dHBzOi8vZGV2ZWxvcGVyLnR3aXR0ZXIuY29tL2VuL2FwcHMifQ%3D%3D%22%7D" >
-                <FaTwitter />
-              </a>
-
+              <div action="#" class="subscribe">
+                <input type="text" class="form-control" placeholder="Enter your e-mail"/>
+                <input className="btnsend" type="submit" class="btn btn-submit" value="Send"/>
+              </div>
             </div>
-            </aside>
-          </div>
-          <div class="footer-top2 col-lg-3 col-md-3 col-sm-3 col-xs-12">
-            <aside id="custom_html-4" class="widget_text widget widget_custom_html"><h5 class="wg-title">Get in Touch</h5><div class="textwidget custom-html-widget"><p>Mehrotra Consumer Products Pvt. Ltd.
-              26G, Sector 31, Ecotech 1, <br />
-              Greater Noida-201308, India</p>
-              <br />
-              <p><a href="mailto:customercare@organictattva.com" class="link-ho">customercare@organictattva.com</a></p>
-              <p><a href="tel:+91 120 4260545" class="link-ho">+91 120 4260545</a></p></div></aside>
-          </div>
-
-          <div class="footer-top3 col-lg-3 col-md-3 col-sm-3 col-xs-12">
-            <aside id="custom_html-3" class="widget_text widget widget_custom_html"><h5 class="wg-title">Quick Links</h5><div class="textwidget custom-html-widget"><p><a href="https://www.organictattva.com/">Trang chủ</a></p>
-             
-              <p><a href="/product">Sản Phẩm</a></p>
-              <p><a href="/contact">Liên Hệ</a></p>
-              <p><a href="/new">Tin Tức</a></p>
-
+            <div class="col-md mb-4 mb-md-0">
+              <h3>Latest Tweet</h3>
+              <ul class="list-unstyled tweets">
+                <li class="d-flex">
+                <FiTwitter /> <div class="mr-4"><span class="icon icon-twitter"></span></div>
+                   <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere unde omnis veniam porro excepturi.</div>
+                </li>
+                <li class="d-flex">
+                <FiTwitter /> <div class="mr-4"><span class="icon icon-twitter"></span></div>
+                  <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere unde omnis veniam porro excepturi.</div>
+                </li>
+                <li class="d-flex">
+                <FiTwitter />  <div class="mr-4"><span class="icon icon-twitter"></span></div>
+                  <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere unde omnis veniam porro excepturi.</div>
+                </li>
+              </ul>
             </div>
-            </aside>
-          </div>
 
-          <div class="footer-top4 col-lg-3 col-md-3 col-sm-3 col-xs-12">
-            <aside id="custom_html-2" class="widget_text widget widget_custom_html"><h5 class="wg-title">Useful Links</h5><div class="textwidget custom-html-widget"><p><a href="https://www.organictattva.com/terms-conditions/">Terms &amp; Conditions</a></p>
-              <p><a href="https://www.organictattva.com/privacy-policy/">Privacy Policy</a></p>
-              <p><a href="https://www.organictattva.com/sitemap">Sitemap</a></p>
-              <p><a href="https://www.organictattva.com/faqs/">FAQ's</a></p>
-              <p><a href="https://www.organictattva.com/buy-now/">Buy Now</a></p></div></aside>
-          </div>
 
+            <div class="col-md-3 mb-4 mb-md-0">
+              <h3><AiFillInstagram />Instagram</h3>
+              <div class="row gallery">
+                <div class="col-6">
+                  <a href="https://www.instagram.com/"><img src={image1} alt="Image" class="img-fluid"/></a>
+                  <a href="https://www.instagram.com/"><img src={image3} alt="Image" class="img-fluid"/></a>
+                </div>
+                <div class="col-6">
+                  <a href="https://www.instagram.com/"><img src={image2} alt="Image" class="img-fluid"/></a>
+                  <a href="https://www.instagram.com/"><img src={image3} alt="Image" class="img-fluid"/></a>
+                </div>
+              </div>
+            </div>
+            
+            <div class="col-12">
+              <div class="py-5 footer-menu-wrap d-md-flex align-items-center">
+                <ul class="list-unstyled footer-menu mr-auto">
+                  <li><a href="">Home</a></li>
+                  <li><a href="#">About</a></li>
+                  <li><a href="#">Our works</a></li>
+                  <li><a href="#">Services</a></li>
+                  <li><a href="#">Blog</a></li>
+                  <li><a href="#">Contacts</a></li>
+                </ul>
+                <div class="site-logo-wrap ml-auto">
+                  <a href="#" class="site-logo">
+                    Colorlib
+                  </a>
+                </div>
+              </div>
+            </div>
+            
+          </div>
         </div>
       </div>
+    </footer>
+
+
+
+      
     </div>
 
   );
