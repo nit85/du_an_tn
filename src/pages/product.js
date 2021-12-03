07c978b6acd8ca6198f25e260bc4ffcf894ec105
tai <thironman85@gmail.com>
2021-12-03 20:30:59 +0700

    commit

--- src/pages/product.js
+++ src/pages/product.js
@@ -8,6 +8,7 @@ import image3 from '../images/slide3.jpg'
 const Product = () => {
   return (
     <div >
+      <div>
       <div className="timkiemsp">
       <form class="d-flex">
         <input class="form-control me-2" type="search" placeholder="Tìm kiếm sản phẩm" aria-label="Search"/>
@@ -174,6 +175,25 @@ const Product = () => {
         </Button>
       </CardActions>
     </Card>
+    </div>
+
+    <br/>
+    <br/>
+
+
+ <nav aria-label="Page navigation example">
+  <ul class="pagination justify-content-center">
+    <li class="page-item disabled">
+      <a class="page-link">Previous</a>
+    </li>
+    <li class="page-item"><a class="page-link" href="#">1</a></li>
+    <li class="page-item"><a class="page-link" href="#">2</a></li>
+    <li class="page-item"><a class="page-link" href="#">3</a></li>
+    <li class="page-item">
+      <a class="page-link" href="#">Next</a>
+    </li>
+  </ul>
+</nav>
 
 
     </div>
